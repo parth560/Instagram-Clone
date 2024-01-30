a3553de73760627400180e9f4126da581db7935a
parth650 <130958174+parth650@users.noreply.github.com>
2024-01-30 01:20:08 -0800

    Added Update in posts.js

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -46,4 +46,28 @@ export const getFeedPosts = async (req, res) => {
     }
   };
   
+  /* UPDATE */
+  export const likePost = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { userId } = req.body;
+      const post = await Post.findById(id);
+      const isLiked = post.likes.get(userId);
   
+      if (isLiked) {
+        post.likes.delete(userId);
+      } else {
+        post.likes.set(userId, true);
+      }
+  
+      const updatedPost = await Post.findByIdAndUpdate(
+        id,
+        { likes: post.likes },
+        { new: true }
+      );
+  
+      res.status(200).json(updatedPost);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
