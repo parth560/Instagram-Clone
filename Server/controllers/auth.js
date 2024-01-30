0bc300099176dafb754bcc74fc02f1ecea38cd9e
parth650 <130958174+parth650@users.noreply.github.com>
2024-01-30 01:17:03 -0800

    Added logging function in auth.js

--- Server/controllers/auth.js
+++ Server/controllers/auth.js
@@ -21,8 +21,8 @@ export const register = async(req, res) => {
          const passwordHash = await bcrypt.hash(password, salt);
 
          const newUser = new User({
-            firstname,
-            lastname,
+            firstName,
+            lastName,
             email,
             password: passwordHash,
             picturePath,
@@ -39,4 +39,23 @@ export const register = async(req, res) => {
         res.status(500).json({error: err.message});
 
     }
-}
+};
+
+/* Logging */
+
+export const login = async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email: email });
+      if (!user) return res.status(400).json({ msg: "User does not exist. " });
+  
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
+  
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      delete user.password;
+      res.status(200).json({ token, user });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  };
