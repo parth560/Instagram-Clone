62343a5e6dfa7a5416ada70194525de857bb0712
parth650 <130958174+parth650@users.noreply.github.com>
2024-01-30 01:19:15 -0800

    Added Read function in posts.js

--- Server/controllers/posts.js
+++ Server/controllers/posts.js
@@ -26,3 +26,24 @@ export const createPost = async (req, res) => {
   }
 };
 
+/* READ */
+export const getFeedPosts = async (req, res) => {
+    try {
+      const post = await Post.find();
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+  
+  export const getUserPosts = async (req, res) => {
+    try {
+      const { userId } = req.params;
+      const post = await Post.find({ userId });
+      res.status(200).json(post);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
+  };
+  
+  
