4f7a39eb45e51cf7fffcebae46acd573f66bccb3
parth650 <130958174+parth650@users.noreply.github.com>
2024-01-30 03:01:00 -0800

    Added modules and middleware

--- Server/index.js
+++ Server/index.js
@@ -43,6 +43,7 @@ const upload = multer({storage});
 /* Routes with files */
 
 app.post("/auth/register", upload.single("picture"), register);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
